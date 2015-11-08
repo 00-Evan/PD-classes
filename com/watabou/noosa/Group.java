4712ec73bf451215fd3062d6b9f7f52a1cd4f637
Evan Debenham <Evan.SHPX@gmail.com>
2015-11-07 19:23:20 -0500

    v0.3.2b: fixed some bugs with groups and changed level visuals

--- com/watabou/noosa/Group.java
+++ com/watabou/noosa/Group.java
@@ -38,6 +38,7 @@ public class Group extends Gizmo {
 	
 	@Override
 	public void destroy() {
+		super.destroy();
 		for (int i=0; i < length; i++) {
 			Gizmo g = members.get( i );
 			if (g != null) {
