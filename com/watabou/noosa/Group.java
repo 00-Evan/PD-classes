48308cd26657e5f6eb0a79ab98b9d8bdcb52d0d3
Evan Debenham <Evan.SHPX@gmail.com>
2016-08-10 19:00:12 -0400

    v0.4.1a: added a method to allow adding a gizmo to the front of groups

--- com/watabou/noosa/Group.java
+++ com/watabou/noosa/Group.java
@@ -121,6 +121,36 @@ public class Group extends Gizmo {
 		return g;
 	}
 	
+	public Gizmo addToFront( Gizmo g){
+
+		if (g.parent == this) {
+			return g;
+		}
+
+		if (g.parent != null) {
+			g.parent.remove( g );
+		}
+
+		// Trying to find an empty space for a new member
+		// starts from the front and never goes over a none-null element
+		for (int i=length-1; i >= 0; i--) {
+			if (members.get( i ) == null) {
+				if (i == 0 || members.get(i - 1) != null) {
+					members.set(i, g);
+					g.parent = this;
+					return g;
+				}
+			} else {
+				break;
+			}
+		}
+
+		members.add( g );
+		g.parent = this;
+		length++;
+		return g;
+	}
+	
 	public Gizmo addToBack( Gizmo g ) {
 		
 		if (g.parent == this) {
